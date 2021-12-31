f4b9f906e4dabbbcf0704abf1381b45de0bd905b
Guilherme Fonseca <guilhermesfonsecaa@gmail.com>
2021-12-31 13:40:56 -0300

    raiva

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,36 @@ $(document).ready(function () {
     heightHeader();
 
     function mapa (){
+        if($(window).width() > 1024) {
+            $(document).on('scroll', function() {
+                let scroll = $(window).scrollTop();
+                $('.mapa').css({
+                    '-webkit-transform' : 'translateY(-'+scroll+')',
+                    '-moz-transform'    : 'translateY(-'+scroll+')',
+                    '-ms-transform'     : 'translateY(-'+scroll+')',
+                    '-o-transform'      : 'translateY(-'+scroll+')',
+                    'transform'         : 'translateY(-'+scroll+')'
+                })
+            })
+        }
         $('.mapa__figure path').on('click', function() {
             let municipio = $(this).data('name');
-            $('.info__item').removeClass('show')
-            $('.info__item[data-name="'+municipio+'"]').addClass('show')
-        })
+            $('.info__item').removeClass('show');
+            $('.info__item[data-name="'+municipio+'"]').addClass('show');
+            let positionY = $(this).position().top - $(this).parents().position().top;
+            let positionX = $(this).offset().left - $(this).parents().position().left;
+            console.log('Y:' + positionY, 'X:' + positionX, 'Largura: ' + $('.mapa__figure').width(), 'Altura: ' + $('.mapa__figure').height());
+            $('.info__item.show').css({'top': positionY, 'left': positionX});
+        });
     }
     mapa();
+
+    function accordion () {
+        $('.accordion').on('click', function () {
+            $('.accordion').removeClass('accordion--open');
+            $(this).addClass('accordion--open');
+        });
+    }
+
+    accordion();
 })
